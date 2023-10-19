06bdadcde3b84ff79d16f98d24cccedf1512dcf3
Grillo Pc Oficina <carlsenrmt26@gmail.com>
2023-10-19 11:57:21 -0400

    tabla añadida

--- resources/views/user/registrar.blade.php
+++ resources/views/user/registrar.blade.php
@@ -7,6 +7,8 @@
                 <div class="col-md-8">
                     <div class="card">
                         <div class="card-header">Registro de Asistencia</div>
+
+                        <!-- end row -->
                         <div class="row">
                             <div class="col-lg-12">
                                 <div class="card-body">
@@ -14,49 +16,36 @@
                                     <p class="card-title-desc">
                                         Solamente usted puede realizar el registro de Asistencia de los usuarios.
                                     </p>
-                                        <form method="post" action="{{ route('users.register') }}">
-                                            @csrf
-                                            <div class="row">
-                                                <div class="form-group col-9">
-                                                    <label for="user_id">Usuario:</label>
-                                                    <select name="user_id" id="user_id" class="form-control">
-                                                        @foreach ($users as $user)
-                                                            <option value="{{ $user->id }}">{{ $user->name }}</option>
-                                                        @endforeach
-                                                    </select>
-                                                </div>
-                                                <div class="form-group col-3">
-                                                    <label for="check_in_morning">Fecha:</label>
-                                                    <input class="form-control" type="date" value="{{ now()->toDateString() }}" disabled id="example-date-input">
-                                                </div>
 
-                                            </div>
-                                            
-                                            <div class="row pt-3">
-                                                <div class="form-group col">
-                                                    <label for="check_in_morning">Ingreso Mañana:</label>
-                                                    <input type="time" name="check_in_morning" id="check_in_morning" class="form-control" value="{{ old('check_in_morning') }}">
-                                                </div>
-                                                <div class="form-group col">
-                                                    <label for="check_out_morning">Salida Mañana:</label>
-                                                    <input type="time" name="check_out_morning" id="check_out_morning" class="form-control">
-                                                </div>
-                                                <div class="form-group col">
-                                                    <label for="check_out_morning">Ingreso Tarde:</label>
-                                                    <input type="time" name="check_out_morning" id="check_out_morning" class="form-control">
-                                                </div>
-                                                <div class="form-group col">
-                                                    <label for="check_out_morning">Salida Tarde:</label>
-                                                    <input type="time" name="check_out_morning" id="check_out_morning" class="form-control">
-                                                </div>
-
-                                            </div>
-                                            
-                                            <!-- Agrega más campos según tus necesidades -->
-                                            <div class="card-body text-center">
-                                                <button type="submit" class="btn btn-success waves-effect waves-light">Registrar</button>
-                                            </div>
-                                        </form>
+                                    <div class="table-responsive">
+                                        <table class="table mb-0">
+        
+                                            <thead class="table-light">
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th>Nombre</th>
+                                                    <th>Ingreso Mañana</th>
+                                                    <th>Salida Mañana</th>
+                                                    <th>Ingreso Tarde</th>
+                                                    <th>Salida Tarde</th>
+                                                    <th>Acciones</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                            @foreach ($users as $user)
+                                                <tr>
+                                                    <th scope="row">{{ $user->id}}</th>
+                                                    <td>{{ $user->name }}</td>
+                                                    <td>{{ $user->check_in_morning}}</td>
+                                                    <td>{{ $user->check_out_morning}}</td>
+                                                    <td>{{ $user->check_in_afternoon}}</td>
+                                                    <td>{{ $user->check_out_afternoon}}</td>
+                                                    <td><button type="submit" class="btn btn-success waves-effect waves-light">Registrar</button></td>
+                                                </tr>
+                                            @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>
                             </div>
                         <!-- end row -->
